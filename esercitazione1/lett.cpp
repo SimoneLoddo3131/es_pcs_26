ad8531928994abec4ddf7fcb7e8c7a32aadbfc4a
SimoneLoddo3131 <simoneloddo3107.com>
2026-03-11 13:15:53 +0000

    consegna es1

---
+++ esercitazione1/lett.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	
+	
+	for (int i=0; i< argc; i++){
+		
+		cout << "parametro" << i << "=" << argv[i] << "\n";
+	}
+	return 0;
+}
+
+	
+	
